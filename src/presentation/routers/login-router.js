85a613459927edbc8390bed77459e5fac10ab18d
marcosCorreia <marcos.cjr@hotmail.com>
2020-12-29 10:46:25 -0300

    ensure LoginRouter return a valid token when valid credentials are provided

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -21,6 +21,6 @@ module.exports = class LoginRouter {
     if (!acessToken) {
       return HttpResponse.unauthorizedError()
     }
-    return HttpResponse.ok()
+    return HttpResponse.ok({ acessToken })
   }
 }
