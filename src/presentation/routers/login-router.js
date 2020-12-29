0ecfc872dc28e2848dc0725f993aafb780b3c921
marcosCorreia <marcos.cjr@hotmail.com>
2020-12-29 11:22:08 -0300

    move server error to a helper method

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -6,21 +6,22 @@ module.exports = class LoginRouter {
   }
 
   route (httpRequest) {
-    if (!httpRequest || !httpRequest.body || !this.authUseCase || !this.authUseCase.auth) {
+    try {
+      const { email, password } = httpRequest.body
+      if (!email) {
+        return HttpResponse.badRequest('email')
+      }
+      if (!password) {
+        return HttpResponse.badRequest('password')
+      }
+      const acessToken = this.authUseCase.auth(email, password)
+      if (!acessToken) {
+        return HttpResponse.unauthorizedError()
+      }
+      return HttpResponse.ok({ acessToken })
+    } catch (error) {
+      // console.error(error)
       return HttpResponse.serverError()
     }
-    const { email, password } = httpRequest.body
-    if (!email) {
-      return HttpResponse.badRequest('email')
-    }
-
-    if (!password) {
-      return HttpResponse.badRequest('password')
-    }
-    const acessToken = this.authUseCase.auth(email, password)
-    if (!acessToken) {
-      return HttpResponse.unauthorizedError()
-    }
-    return HttpResponse.ok({ acessToken })
   }
 }
