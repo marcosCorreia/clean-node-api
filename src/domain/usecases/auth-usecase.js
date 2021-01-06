48d4c741ee1bafb4b5adb92a1333f5fde2bd040f
marcosCorreia <marcos.cjr@hotmail.com>
2021-01-05 22:50:58 -0300

    change AuthUseCase constructor to receive an object

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -1,10 +1,10 @@
 const { MissingParamError } = require('../../utils/errors')
 
 module.exports = class AuthUseCase {
-  constructor (loadUserByEmailRepository, encrypter, tokenGeneratorSpy) {
+  constructor ({ loadUserByEmailRepository, encrypter, tokenGenerator }) {
     this.loadUserByEmailRepository = loadUserByEmailRepository
     this.encrypter = encrypter
-    this.tokenGeneratorSpy = tokenGeneratorSpy
+    this.tokenGenerator = tokenGenerator
   }
 
   async auth (email, password) {
@@ -17,7 +17,7 @@ module.exports = class AuthUseCase {
     const user = await this.loadUserByEmailRepository.load(email)
     const isValid = user && await this.encrypter.compare(password, user.password)
     if (isValid) {
-      const accessToken = await this.tokenGeneratorSpy.generate(user.id)
+      const accessToken = await this.tokenGenerator.generate(user.id)
       return accessToken
     }
     return null
